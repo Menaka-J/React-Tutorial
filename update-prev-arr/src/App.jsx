2c79781f2d0ce41f8b6e61efccff081f0ec6b710
Menaka J <menakaraman4@gmail.com>
2025-12-30 16:12:47 +0530

    commit 2

---
+++ update-prev-arr/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Updater from './Updater'
+
+function App() {
+
+  return (
+    <>
+      <Updater></Updater>
+    </>
+  )
+}
+
+export default App
---
+++ update-prev-arr/src/Updater.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+function Updater() {
+
+    const [player, setPlayer] = useState({ name: "", age: 17, team: "" })
+
+    function handleName(e) {
+        setPlayer(p => ({ ...p, name: e.target.value }))
+    }
+
+    function handleAge(e) {
+        setPlayer(p => ({ ...p, age: e.target.value }))
+    }
+
+    function handleTeam(e) {
+        setPlayer(p => ({ ...p, team: e.target.value }))
+    }
+    return (
+
+        <>
+
+        <div className="displaytxt">
+            <p>Name: {player.name}</p>
+            <p>Age: {player.age}</p>
+            <p>Team: {player.team}</p>
+        </div>
+
+        <br /><br />
+
+        <div className="inputtxt">
+            <label>Enter your Name:</label>
+            <br />
+            <input type="text" value={player.name} placeholder="Enter your Name" onChange={handleName} />
+        </div>
+        </>
+    )
+}
+
+export default Updater
