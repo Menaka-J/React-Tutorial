fdcc010eb0415679d0c256dc6e4219f0d032b5b4
Menaka J <menakaraman4@gmail.com>
2025-12-30 16:44:48 +0530

    commit7

--- update-prev-arr/src/ArrayUpdate.jsx
+++ update-prev-arr/src/ArrayUpdate.jsx
@@ -1,8 +1,40 @@
-function ArrayUpdate(){
+import { useState } from "react"
 
+function ArrayUpdate() {
 
-    return(
+    const [teams, setTeam] = useState(["Red Bull", "Ferrari", "Mercedes"])
+
+    function handleaddteam() {
+        const newteam = document.getElementById("io").value;
+        document.getElementById("io").value = "";
+        setTeam(t => ([...t, newteam]));
+    }
+
+    function handledeleteteam(idx) {
+        setTeam(teams.filter((_, index) => idx != index));
+    }
+
+    return (
         <>
+            <div className="rapper">
+                <h2>ARRAY UPDATER EXERCISE </h2>
+
+                <div className="displaytxt">
+                    <h2>TEAMS LIST</h2>
+                    <ul className="listitems">
+                        {teams.map((team, i) =>
+                            <li key={i} onClick={() => handledeleteteam(i)}>{team}</li>
+                        )}
+                    </ul>
+                </div>
+
+                <div className="inputtxt">
+                    <h2>INPUT GETTER </h2>
+                    <input type="text" placeholder="Enter Team Name" id="io"/>
+                    <button className="button1" onClick={handleaddteam}>Add Team</button>
+                </div>
+            </div>
+
         </>
     )
 }
