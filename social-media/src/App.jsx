77042b5e2c0c6568bbfee3b6d828532f6e1cebcd
Menaka J <menakaraman4@gmail.com>
2026-01-04 13:18:39 +0530

    doing social-media

---
+++ social-media/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Header'
+import Nav from './Nav'
+import PostPage from './PostPage'
+import NewPost from './NewPost'
+import About from './About'
+import Missing from './Missing'
+import Footer from './Footer'
+import Home from './Home'
+import { Link, Route, Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/newpost">Newpost</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/newpost' element={<NewPost />} />
+        </Routes>
+
+        {/* <Header></Header>
+        <Nav></Nav>
+        <Home></Home>
+        <NewPost></NewPost>
+        <PostPage></PostPage>
+        <About></About>
+        <Missing></Missing>
+        <Footer></Footer> */}
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ social-media/src/PostPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostPage() {
+    return (
+        <>
+        <h1>PostPage</h1>
+        </>
+    )
+}
+
+export default PostPage 
